35700975481da0c0762c3e00bd91e84c02bb9fdd
Ramūnas Korobačius <ramunas.korobacius@visma.com>
2014-10-09 08:09:24 +0300

    Initial project setup

---
+++ VertexReader/VertexReader.cpp
@@ -0,0 +1,26 @@
+// VertexReader.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+#include <slapi/slapi.h>
+#include <slapi/geometry.h>
+#include <slapi/initialize.h>
+#include <slapi/unicodestring.h>
+#include <slapi/model/model.h>
+#include <slapi/model/entities.h>
+#include <slapi/model/face.h>
+#include <slapi/model/edge.h>
+#include <slapi/model/vertex.h>
+#include <vector>
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+
+	return 0;
+}
+
