8d80eb4793014329cd9a525c5c9addf7128a269c
Ramunas <ramunsk@users.noreply.github.com>
2014-10-10 00:28:57 +0300

    Vertices from faces printed

--- VertexReader/VertexReader.cpp
+++ VertexReader/VertexReader.cpp
@@ -15,12 +15,54 @@
 #include <slapi/model/vertex.h>
 #include <vector>
 
+using namespace std;
 
+SUModelRef _model = SU_INVALID;
+SUEntitiesRef _entities = SU_INVALID;
+vector<SUFaceRef> _faces;
+vector<SUVertexRef> _vertices;
+
+void printVertices()
+{
+	size_t i;
+	for (i = 0; i < _vertices.size(); i++)
+	{
+		SUPoint3D point;
+		SUResult result = SUVertexGetPosition(_vertices[i], &point);
+
+		printf("{ %.1ff, %.1ff, %.1ff}\n", point.x, point.y, point.z);
+	}
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	size_t i, j;
+	SUResult result;
 
+	SUInitialize();
+
+	result = SUModelCreateFromFile(&_model, "sample.skp");
+	result = SUModelGetEntities(_model, &_entities);
+
+	size_t faceCount = 0;
+	result = SUEntitiesGetNumFaces(_entities, &faceCount);
+	
+	_faces = vector<SUFaceRef>(faceCount);
+	result = SUEntitiesGetFaces(_entities, faceCount, &_faces[0], &faceCount);
+
+	for (i = 0; i < faceCount; i++){
+		size_t vertexCount;
+		result = SUFaceGetNumVertices(_faces[i], &vertexCount);
+
+		_vertices.resize(_vertices.size() + vertexCount);
+		result = SUFaceGetVertices(_faces[i], vertexCount, &_vertices[_vertices.size() - vertexCount], &vertexCount);
+	}
+
+	printVertices();
 
 	return 0;
 }
+
+
+
 
