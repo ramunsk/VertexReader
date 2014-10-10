1e6c85dbfef62a08182b91e45789bc08606e05a0
Ramūnas Korobačius <ramunas.korobacius@visma.com>
2014-10-10 13:40:41 +0300

    Points are sorted and unique now

--- VertexReader/VertexReader.cpp
+++ VertexReader/VertexReader.cpp
@@ -14,6 +14,7 @@
 #include <slapi/model/edge.h>
 #include <slapi/model/vertex.h>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -21,12 +22,28 @@ SUModelRef _model = SU_INVALID;
 SUEntitiesRef _entities = SU_INVALID;
 vector<SUFaceRef> _faces;
 vector<SUVertexRef> _vertices;
+vector<SUEdgeRef> _edges;
+
+
+
+bool operator<(const SUVertexRef &a, const SUVertexRef &b)
+{
+    return a.ptr < b.ptr;
+}
+
+bool operator==(const SUVertexRef &a, const SUVertexRef &b)
+{
+    return a.ptr == b.ptr;
+}
 
 void printVertices()
 {
 	size_t i;
 	for (i = 0; i < _vertices.size(); i++)
 	{
+        std::sort(_vertices.begin(), _vertices.end());
+        _vertices.erase(unique(_vertices.begin(), _vertices.end()), _vertices.end());
+
 		SUPoint3D point;
 		SUResult result = SUVertexGetPosition(_vertices[i], &point);
 
@@ -57,6 +74,24 @@ int _tmain(int argc, _TCHAR* argv[])
 		_vertices.resize(_vertices.size() + vertexCount);
 		result = SUFaceGetVertices(_faces[i], vertexCount, &_vertices[_vertices.size() - vertexCount], &vertexCount);
 	}
+
+    size_t edgeCount = 0;
+    result = SUEntitiesGetNumEdges(_entities, false, &edgeCount);
+
+    _edges = vector<SUEdgeRef>(edgeCount);
+    result = SUEntitiesGetEdges(_entities, false, edgeCount, &_edges[0], &edgeCount);
+
+    for (i = 0; i < edgeCount; i++){
+        
+        SUVertexRef startVertex = SU_INVALID;
+        result = SUEdgeGetStartVertex(_edges[i], &startVertex);
+        _vertices.push_back(startVertex);
+
+        SUVertexRef endVertex = SU_INVALID;
+        result = SUEdgeGetEndVertex(_edges[i], &endVertex);
+        _vertices.push_back(endVertex);
+
+    }
 
 	printVertices();
 
